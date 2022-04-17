ed945dbeb689172a10845db89bd4fdb060bc37ba
Alton Liew <101538978+pedestrianlau@users.noreply.github.com>
2022-04-18 00:10:02 +0800

    Update attackvector11.py

--- ATTACKVECTOR/attackvector11.py
+++ ATTACKVECTOR/attackvector11.py
@@ -16,118 +16,79 @@ import os
 
 
 class AttackVectorEleven(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        framefont = tkfont.Font(family='Calibri', size=33, weight="bold")
-
-        main_frame = tk.Frame(self)
-
-        highlightFont = font.Font(family='Calibri', name='appHighlightFont11', size=18)
-
-        global unpatchedvul
-        unpatchedvul = tk.PhotoImage(file='resources/unpatched.png')
-
-        def load_nmap_tool():
-            os.chdir("./Tools/Nmap_Gui")
-            cmd = "python3 nmapGUI.py"
-            p1 = Popen(cmd, stdout=PIPE, universal_newlines=True, shell=True)
-            os.chdir("../../")
-
-        def load_vulnexploit_tool():
-            os.chdir("./Tools")
-            cmd = 'exo-open --launch TerminalEmulator'
-            p1 = Popen([cmd + ' python3 VulnExploit.py'], stdout=PIPE, shell=True)
-            os.chdir("../../")
-
-        def load_terminal():
-            p1 = Popen("exo-open --launch TerminalEmulator", stdout=PIPE, universal_newlines=True, shell=True).stdout
-
-        def change_to_Step1():
-            text = (
-                "\n\nStep 1: \n\n"
-                "o   Recon the target system for Vulnerabilities\n\n"
-                "o   Using Nmap tool below, Enter the IP Address of the target system and select Vulscan from the scan type.\n\n"
-                "o   Press Scan           Or\n\n"
-                "o   Open a new terminal and enter the script below - changing IP to the IP of the target system.\n\n"
-                "               nmap -sV --script=Tools/Nmap_Gui/scipag_vulscan/vulscan.nse IP\n"
-            )
-            step1frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
-                                    aspect=350)
-            step1frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
-            nmapButton = tk.Button(step1frame, text="Nmap Tool", bg="#E7E7E7", fg="black", font=highlightFont,
-                                   command=load_nmap_tool, relief='flat').place(rely=0.5, relx=0.02, relheight=0.05,
-                                                                                relwidth=0.1)
-            terminalButton = tk.Button(step1frame, text="Terminal", bg="#E7E7E7", fg="black", font=highlightFont,
-                                       command=load_terminal, relief='flat').place(rely=0.5, relx=0.14, relheight=0.05,
-                                                                                   relwidth=0.1)
-
-        def change_to_Step2():
-            text = (
-                "\n\nStep 2:  \n\n"
-                "o   Use ‘UpdateScript.py’from Attack Vector 1 if you have not used it\n\n"
-                "o   Press VulnExploit Tool button\n\n"
-                "o   Select 1 to search Metasploit for vulnerability found in step 1\n\n"
-                "o   Copy the exploit found\n\n"
-                "o   Select 2 to find information about required fields of an exploit and paste a copied exploit from select 1.\n\n"
-
-            )
-            step1frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
-                                    aspect=300)
-            step1frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
-            vulnButton = tk.Button(step1frame, text="VulnExploit", bg="#E7E7E7", fg="black", font=highlightFont,
-                                   command=load_vulnexploit_tool, relief='flat').place(rely=0.6, relx=0.02,
-                                                                                       relheight=0.05, relwidth=0.1)
-            terminalButton = tk.Button(step1frame, text="Terminal", bg="#E7E7E7", fg="black", font=highlightFont,
-                                       command=load_terminal, relief='flat').place(rely=0.6, relx=0.14, relheight=0.05,
-                                                                                   relwidth=0.1)
-
-        def change_to_Step3():
-            text = (
-                "\n\nStep 3:  \n\n"
-                "o   Select 3 to use exploit, enter exploit that you searched information about and enter requested information about the exploit and payload.\n\n"
-                "o   Press Enter to launch exploit\n\n"
-                "o   Select 4 open session created by exploit\n\n"
-                "o   Select 5 to exit program\n\n"
-            )
-            step1frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
-                                    aspect=300)
-            step1frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
-            vulnButton = tk.Button(step1frame, text="VulnExploit", bg="#E7E7E7", fg="black", font=highlightFont,
-                                   command=load_vulnexploit_tool, relief='flat').place(rely=0.6, relx=0.02,
-                                                                                       relheight=0.05, relwidth=0.1)
-            terminalButton = tk.Button(step1frame, text="Terminal", bg="#E7E7E7", fg="black", font=highlightFont,
-                                       command=load_terminal, relief='flat').place(rely=0.6, relx=0.14, relheight=0.05,
-                                                                                   relwidth=0.1)
-
+  
+  def __init__(self, parent, controller):
+    tk.Frame.__init__(self, parent)
+    self.controller = controller
+    framefont = tkfont.Font(family='Calibri', size=33, weight="bold")
+    
+    main_frame = tk.Frame(self)
+    
+    highlightFont = font.Font(family='Calibri', name='appHighlightFont11', size=18)
+    
+    def change_to_Step1():
+      text = (
+        "\nStep 1: Setup environment (the following setup was used in this demonstration)\n\n"
+        "o   phpMyAdmin 4.8.1\n\n"
+        "o   Windows 7\n\n"
+        "o   Kali Linux\n\n"
+        "o   XAMPP 7.2.0 (used to run phpMyAdmin on the Windows 7 machine)\n\n"
+        "o   Metasploit\n\n"
+        "o   Windows 7 and Kali Linux is configured to be in the same internal network.\n\n"
+      )
+      step1frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
+                              aspect=300)
+      step1frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
+      
+      def change_to_Step2():
+        text = (
+          "\nStep 2: Walkthrough\n\n"
+          "o   On the windows machine, make sure that you are running and logged into phpMyAdmin.\n\n"
+          "o   Open terminal on Kali Linux and run metaploit using this command:\n"
+          "     msfconsole\n\n"
+          "o   Load the exploit using:\n"
+          "     use exploit/multi/http/phpmyadmin_lfi_rce\n\n"
+          "o   You are now required to enter the RHOSTS (Victim's IP address)\n"
+          "and TARGETURI (name of the folder you have installed phpmyadmin 4.8.1 in).\n\n"
+          "o   Use the following commands to set RHOSTS and TARGETURI:\n"
+          "     set RHOSTS 192.168.68.117\n"
+          "     set TARGETURI /phpmyadmin4.8.1/\n\n"
+          "o   (IP address and folder name is just and example. Please modify accordingly)\n\n"
+          "o   Finally run the exploit using:\n"
+          "     run\n\n"
+          "o   If successfull, a meterpreter session should be created and the RCE attack is complete.\n\n"
+        )
+        step2frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
+                                aspect=300)
+        step2frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
+        
         # creates blue bar as canvas below nav bar housing label containing title of page
         title_canvas = tk.Canvas(self, bg='#64C1DA', highlightthickness=0)
         title_canvas.place(rely=0.08, relheight=0.12, relwidth=1)
-        title_label = tk.Label(self, text="Unpatched Vulnerabilities and Exploits", bg='#4D6C84', fg='white',
-                               anchor="c", font=framefont)
+        title_label = tk.Label(self, text="phpMyAdmin 4.8.1 RCE", bg='#4D6C84', fg='white', anchor="c",
+                               font=framefont)
         title_label.place(rely=0.08, relheight=0.12, relwidth=1)
-
+        
         allscreenframe = tk.Label(main_frame, bg='white')
         allscreenframe.place(rely=0.2, relheight=1, relwidth=1)
-
-        sidescreenframe = tk.Label(main_frame, text="\n           Steps", bg='#E7E7E7', font=('Calibri', 20, 'bold'),
+                                 
+        sidescreenframe = tk.Label(main_frame, text="\n         Steps", bg='#E7E7E7', font=('Calibri', 20),
                                    anchor='nw')
         sidescreenframe.place(rely=0.2, relheight=1, relwidth=0.2)
-
-        unpatched_label = tk.Label(main_frame, image=unpatchedvul)
-        unpatched_label.place(rely=0.36, relx=0.4)
-
+        
+        # buttons
         step1btn = tk.Button(main_frame, text="Step 1", bg="#E7E7E7", fg="black", font=highlightFont,
                              command=change_to_Step1, relief='flat').place(rely=0.3, relx=0.02, relheight=0.05,
                                                                            relwidth=0.1)
         step2btn = tk.Button(main_frame, text="Step 2", bg="#E7E7E7", fg="black", font=highlightFont,
-                             command=change_to_Step2, relief='flat').place(rely=0.4, relx=0.02, relheight=0.05,
+                             command=change_to_Step2, relief='flat').place(rely=0.3, relx=0.02, relheight=0.05,
                                                                            relwidth=0.1)
-        step3btn = tk.Button(main_frame, text="Step 3", bg="#E7E7E7", fg="black", font=highlightFont,
-                             command=change_to_Step3, relief='flat').place(rely=0.5, relx=0.02, relheight=0.05,
-                                                                           relwidth=0.1)
-        # step4btn = tk.Button(main_frame, text="Step 4", bg="#E7E7E7", fg="black", font=highlightFont, command=change_to_Step4, relief='flat').place(rely=0.6, relx=0.02, relheight=0.05, relwidth=0.1)
-
+        #step1button = tk.Button(main_frame, text="Step 1", font=highlightFont,
+                                #command=change_to_Step1).place(rely=0.3, relx=0.02, relheight=0.05,
+                                                               #relwidth=0.1)
+        #step2button = tk.Button(main_frame, text="Step 2", font=highlightFont,
+                                #command=change_to_Step2).place(rely=0.4, relx=0.02, relheight=0.05,
+                                                               #relwidth=0.1)
+        
         display_nav_bar(self, controller)
         main_frame.pack(fill='both', expand=1)
