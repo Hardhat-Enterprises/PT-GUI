bc807b7d6b661972944a8392476eb5bda2bc5e08
zinchenkoboris <101746727+zinchenkoboris@users.noreply.github.com>
2022-05-24 04:55:10 +1000

    Add files via upload

--- ATTACKVECTOR/attackvector7.py
+++ ATTACKVECTOR/attackvector7.py
@@ -20,11 +20,11 @@ class AttackVectorSeven(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        framefont = tkfont.Font(family='Calibri', size=33, weight="bold")
+        framefont = tkfont.Font(family='Arial Rounded MT Bold', size=28, weight='bold')
 
         main_frame = tk.Frame(self)
 
-        highlightFont = font.Font(family='Calibri', name='appHighlightFont7', size=18)
+        highlightFont = font.Font(family='OpenSans', name='appHighlightFont7', size=18)
 
         def load_terminal():
             p1 = Popen("exo-open --launch TerminalEmulator", stdout=PIPE, universal_newlines=True, shell=True).stdout
@@ -38,12 +38,9 @@ class AttackVectorSeven(tk.Frame):
                 "o   Mount your new directory to the share using the command 'mount -t nfs targetIP:share /PathToYourNewDirectory -nolock'.\n\n"
                 "o   Check the directory or files you mounted, if you can't see it, exit the directory and access it again.\n\n"
             )
-            step1frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
+            step1frame = tk.Message(main_frame, text=text, font=('OpenSans', 14), anchor='nw',
                                     aspect=300)
-            step1frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
-            terminalButton = tk.Button(step1frame, text="Terminal", bg="#E7E7E7", fg="black", font=highlightFont,
-                                       command=load_terminal, relief='flat').place(rely=0.5, relx=0.02, relheight=0.05,
-                                                                                   relwidth=0.1)
+            step1frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)            
 
         def change_to_Step2():
             text = (
@@ -53,46 +50,55 @@ class AttackVectorSeven(tk.Frame):
                 "o   After copying the id_rsa file, use ssh to connect to that server with the key you obtained. the command will look like this ' ssh -i [Key file(id_rsa)] username@serverIP '.\n\n"
                 "o   You should now have access to the server, but with limited permissions..\n\n"
             )
-            step1frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
+            step1frame = tk.Message(main_frame, text=text, font=('OpenSans', 14), anchor='nw',
                                     aspect=300)
             step1frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
 
         def change_to_Step3():
             text = (
-                "\Step 3: Privilege escalation\n\n"
+                "\nStep 3: Privilege escalation\n\n"
                 "o   As you now have access to the NFS server, make a copy of the /bin/bash to the mounted path. This command will copy it to your current path[cp /bin/bash .]\n\n"
                 "o   In your attack machine, log as root, and copy that bash file from the mounted to your local machine. Remove the copy in the mount, then place move the copy in your local machine back to the mount file.\n\n"
                 "o   This basically creates the bash from the server as a root user. Once you put the bash file back to mounted path, use this command as root [chmod +sx bash].\n\n"
                 "o   Now log back to the server using the ssh command from step two, run the bash file in the mounted path using the command [./bash -p].\n\n"
                 "o   You now have root permissions, use id or whoami commands to confirm"
             )
-            step3frame = tk.Message(main_frame, text=text, fg='black', bg='white', font=('Calibri', 20), anchor='nw',
+            step3frame = tk.Message(main_frame, text=text, font=('OpenSans', 14), anchor='nw',
                                     aspect=300)
             step3frame.place(rely=0.2, relx=0.2, relheight=1, relwidth=1)
 
         # creates blue bar as canvas below nav bar housing label containing title of page
-        title_canvas = tk.Canvas(self, bg='#64C1DA', highlightthickness=0)
-        title_canvas.place(rely=0.08, relheight=0.12, relwidth=1)
-        title_label = tk.Label(self, text="NFS Privilege Escalation", bg='#4D6C84', fg='white', anchor="c",
-                               font=framefont)
-        title_label.place(rely=0.08, relheight=0.12, relwidth=1)
+        title_label = tk.Label(self, text="NFS Privilege Escalation", bg='white', fg='#92CEFF',
+                               anchor="c", font=framefont)
+        title_label.place(rely=0.06, relheight=0.12, relwidth=1)
+        
+        # creates blue bar as canvas below nav bar
+        title_canvas = tk.Canvas(self, bg='#c8e6ff', highlightthickness=0)
+        title_canvas.place(rely=0.08, relheight=0.004, relwidth=1)
 
-        allscreenframe = tk.Label(main_frame, bg='white')
+        # creates blue bar as canvas below title
+        title_canvas = tk.Canvas(self, bg='#c8e6ff', highlightthickness=0)
+        title_canvas.place(rely=0.155, relheight=0.004, relwidth=1)
+
+        allscreenframe = tk.Label(main_frame)
         allscreenframe.place(rely=0.2, relheight=1, relwidth=1)
 
-        sidescreenframe = tk.Label(main_frame, text="\n            Steps", bg='#E7E7E7', font=('Calibri', 20),
+        sidescreenframe = tk.Label(main_frame, text="\n        Start Here:", font=('OpenSans', 18, 'italic'),
                                    anchor='nw')
-        sidescreenframe.place(rely=0.2, relheight=1, relwidth=0.2)
 
-        step1btn = tk.Button(main_frame, text="Step 1", bg="#E7E7E7", fg="black", font=highlightFont,
-                             command=change_to_Step1, relief='flat').place(rely=0.3, relx=0.02, relheight=0.05,
-                                                                           relwidth=0.1)
-        step2btn = tk.Button(main_frame, text="Step 2", bg="#E7E7E7", fg="black", font=highlightFont,
-                             command=change_to_Step2, relief='flat').place(rely=0.4, relx=0.02, relheight=0.05,
-                                                                           relwidth=0.1)
-        step3btn = tk.Button(main_frame, text="Step 3", bg="#E7E7E7", fg="black", font=highlightFont,
-                             command=change_to_Step3, relief='flat').place(rely=0.5, relx=0.02, relheight=0.05,
-                                                                           relwidth=0.1)
+        sidescreenframe.place(rely=0.2, relheight=1, relwidth=0.2)
+        step1btn = ttk.Button(main_frame, text="Step 1", 
+                             command=change_to_Step1, style='Accent.TButton').place(rely=0.3, 
+                             relx=0.02, relheight=0.05, relwidth=0.1)
+        step2btn = ttk.Button(main_frame, text="Step 2", 
+                             command=change_to_Step2, style='Accent.TButton').place(rely=0.4, 
+                             relx=0.02, relheight=0.05, relwidth=0.1)
+        step3btn = ttk.Button(main_frame, text="Step 3", 
+                             command=change_to_Step3, style='Accent.TButton').place(rely=0.5, 
+                             relx=0.02, relheight=0.05, relwidth=0.1)
+        terminalButton = ttk.Button(main_frame, text="Open Terminal", 
+                             command=load_terminal, style='Rocket.TButton').place(rely=0.64, 
+                             relx=0.02, relheight=0.05, relwidth=0.1)
 
         display_nav_bar(self, controller)
         main_frame.pack(fill='both', expand=1)
