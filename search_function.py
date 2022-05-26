ae89d25f8d3b7a765e71cde9fa634bd78802dfbe
KeeganGray <63983002+KeeganGray@users.noreply.github.com>
2022-05-26 14:20:11 +1000

    search function update
    indirect sequential matches

--- search_function.py
+++ search_function.py
@@ -21,11 +21,10 @@ class search:
     "Initial Access Tools", "SSH Bruteforce", "Password Hash Cracker", "ZIP File Brute Forcer", #Initial Access
     "Payloads", "Msfvenom Payload Generator", #Payloads
     "Resource Development Tools", "Notepad", #Resource Dev
+    "Cryptography Tools", "Encryption", "Encoding", "Hashing", #Cryptography Tools
     "Help", "Command Prompt", "API Key Handler", #Help
     #Walkthroughs
     "Buffer Overflow"]
-
-
 
     searchlinks = [#pages
     "Page", "Page", "Page", "Page", "Page", "Page", 
@@ -38,12 +37,11 @@ class search:
     "Page", "Tool", 
     "Page", "Tool", "Tool", "Tool", 
     "Page", "Tool", 
-    "Page", "Tool", 
+    "Page", "Tool",
+    "Page", "Tool", "Tool", "Tool",
     "Page", "Tool", "Tool", 
     #Walkthroughs
     "Walkthrough"]
-
-
 
     pagelinks = [#pages
     "StartPage", "AboutPage", "VectorsPage", "ToolsPage", "WalkthroughClass", "ReferencesPage",
@@ -60,36 +58,55 @@ class search:
     "ToolsPage", "SshBrute", "PHCracker", "ZipBF", #Initial Access
     "ToolsPage", "MsfPayloadGen", #Payloads
     "ToolsPage", "ToolsPage", #Resource Dev... notepad redirects to tools page
+    "ToolsPage", "Encryption", "Encoding", "Hashing",
     "ToolsPage", "Command Prompt", "API_Keys", #Help... cmd prompt redirects to tools page
     #Walkthroughs
     "WalkthroughClass"]
 
-
 searchlists = search
-quicksearch = []
+quicksearch = [] #for direct sequential matches, e.g. search for "too" yields "tools"
+quicksearch2 = [] #for indirect sequential matches, e.g. search for "canne" yields "port scanner"
 
+##on enter key pressed in the search bar, may build a search page
 def build_search(event, search_field, search_canvas, controller):
+    ##serves as the frame. is a canvas that covers the frame (whereever user searched from)
+    ##is displaced by search_leave()... 
     search_canvas.place(relx=0, rely=0.08, relheight=0.92, relwidth=1)
     search_canvas.tk.call('raise', search_canvas._w)
-    input = search_field.get()
 
-    shodanframe = tk.Label(search_canvas, text=input, bg='#3B5262', fg='white', anchor="c")
-    shodanframe.place(rely=0.08, relheight=0.12, relwidth=1)
+    ##this is the input from user search
+    input = search_field.get()
+    
+    #proof of concept to be able to dynamically pass the "search" input variable to page content.
+    templabel = tk.Label(search_canvas, text=input, bg='#3B5262', fg='white', anchor="c")
+    templabel.place(rely=0.08, relheight=0.12, relwidth=1)
+
 
 def clean_quicksearch():
     for term in quicksearch:
         term.place_forget()
     while (len(quicksearch) > 0):
         quicksearch.pop()
+    for term in quicksearch2:
+        term.place_forget()
+    while (len(quicksearch2)>0):
+        quicksearch2.pop()
 
 def search_check(input, search_field, frame, search_canvas, controller):
+    lowest=0
     for idx, entry in enumerate(searchlists.searchterms):
         if(len(input) <= len(entry)):
             if(entry[0:len(input)].lower() == input.lower()): #if the first (however many characters) matches a search term (case insensitive)
                 tempidx = idx
                 quicksearch.append(ttk.Button(frame,text=entry, style="Accent.TButton", command = lambda i=tempidx: [search_field.delete(0, 'end'), search_leave(search_canvas), controller.show_frame(searchlists.pagelinks[i]), clean_quicksearch()])) #make a quicksearch button
                 quicksearch[len(quicksearch)-1].place(rely=0.04 + (len(quicksearch)*0.04), relx=1 - 0.11 * 7 - 7 * 0.01) #place it under search bar
-
+            elif(len(input)>1): 
+                tempidx = idx
+                for x in range (len(entry)-len(input)): #if searched item < 1, looks for indirect sequential matches, e.g. search for "canne" yields "port scanner"
+                    if(entry[x+1:x+1+len(input)].lower() == input.lower()):
+                        quicksearch2.append(ttk.Button(frame,text=entry, style="Accent.TButton", command = lambda i=tempidx: [search_field.delete(0, 'end'), search_leave(search_canvas), controller.show_frame(searchlists.pagelinks[i]), clean_quicksearch()])) #make a quicksearch button
+    for idx, entry in enumerate(quicksearch2):
+        quicksearch2[idx].place(rely=0.04 + ((len(quicksearch)+1*0.04) + idx*0.04), relx=1 - 0.11 * 7 - 7 * 0.01) #place it under search bar
 def search_event(event, search_field, frame, search_canvas, controller):
     #previous suggestions are removed
     clean_quicksearch()
@@ -99,7 +116,7 @@ def search_event(event, search_field, frame, search_canvas, controller):
     if(len(input)>0):
         search_check(input, search_field, frame, search_canvas, controller) #searches
 
-#replace with simple .place forget
+#replace with simple .place forget... navbar MUST call this when changing page away from a searchpage (search_canvas in build_search)
 def search_leave(search_canvas):
     search_canvas.place_forget()
     search_canvas.delete("all")
